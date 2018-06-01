a49496a72d1e84518ba49d4d526ded07080b1d0b
Grad Vasile - SINFL 2015 <grad_simplu@yahoo.com>
2018-06-01 18:36:06 +0300

    Revert "Styling the entire app"
    This reverts commit b2be98b9a5365869527ae845453357284b494c67.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
 	before_action :find_book, only: [:show, :edit, :update, :destroy]
-	before_action :authenticate_user!, only: [:new, :edit]
 
 	def index
 		if params[:category].blank?
